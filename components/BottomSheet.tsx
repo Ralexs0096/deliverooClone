4cec4bf278d4c6fa5923f251d50a587a330cb957
Alexs Ruiz <ralexs.acu@gmail.com>
2023-09-28 01:40:29 -0600

    create bottomSheet body
    - add the necessary elements and styles

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -1,18 +1,139 @@
-import React, { forwardRef, useMemo } from 'react';
-import { Text, View } from 'react-native';
-import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import React, { forwardRef, useCallback, useMemo } from 'react';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  useBottomSheetModal
+} from '@gorhom/bottom-sheet';
+import Colors from '@/constants/Colors';
+import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export type Ref = BottomSheetModal;
 
 const BottomSheet = forwardRef<Ref>((props, ref) => {
   const snapPoints = useMemo(() => ['50%'], []);
+  const renderBackdrop = useCallback(
+    (props: any) => (
+      <BottomSheetBackdrop
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+        {...props}
+      />
+    ),
+    []
+  );
+  const { dismiss } = useBottomSheetModal();
+
   return (
-    <BottomSheetModal ref={ref} snapPoints={snapPoints}>
-      <View>
-        <Text>Bottom Sheet</Text>
+    <BottomSheetModal
+      handleIndicatorStyle={{ display: 'none' }}
+      backgroundStyle={styles.bottomSheetModalStyle}
+      ref={ref}
+      snapPoints={snapPoints}
+      overDragResistanceFactor={0}
+      backdropComponent={renderBackdrop}
+    >
+      <View style={styles.toggle}>
+        <TouchableOpacity style={styles.toggleActive}>
+          <Text style={styles.activeText}>Delivery</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.toggleInactive}>
+          <Text style={styles.inactiveText}>Pickup</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.subheader}>Your Location</Text>
+      <Link href={'/'} asChild>
+        <TouchableOpacity>
+          <View style={styles.item}>
+            <Ionicons name="location-outline" size={20} color={Colors.medium} />
+            <Text style={{ flex: 1 }}>Current Location</Text>
+            <Ionicons name="chevron-forward" size={20} color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+      </Link>
+      <Text style={styles.subheader}>Arrival Time</Text>
+      <Link href={'/'} asChild>
+        <TouchableOpacity>
+          <View style={styles.item}>
+            <Ionicons
+              name="stopwatch-outline"
+              size={20}
+              color={Colors.medium}
+            />
+            <Text style={{ flex: 1 }}>Now</Text>
+            <Ionicons name="chevron-forward" size={20} color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+      </Link>
+
+      <View style={styles.contentContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => dismiss()}>
+          <Text style={styles.buttonText}>Confirm</Text>
+        </TouchableOpacity>
       </View>
     </BottomSheetModal>
   );
+});
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  button: {
+    backgroundColor: Colors.primary,
+    padding: 16,
+    margin: 10,
+    borderRadius: 4,
+    alignItems: 'center'
+  },
+  bottomSheetModalStyle: {
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 0
+  },
+  activeText: {
+    color: '#fff',
+    fontWeight: '700'
+  },
+  inactiveText: {
+    color: Colors.primary
+  },
+  toggle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 32,
+    marginBottom: 32
+  },
+  toggleActive: {
+    backgroundColor: Colors.primary,
+    padding: 8,
+    borderRadius: 32,
+    paddingHorizontal: 30
+  },
+  toggleInactive: {
+    padding: 8,
+    borderRadius: 32,
+    paddingHorizontal: 30
+  },
+  subheader: {
+    fontSize: 16,
+    fontWeight: '600',
+    margin: 16
+  },
+  item: {
+    flexDirection: 'row',
+    gap: 8,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 16,
+    borderColor: Colors.grey,
+    borderWidth: 1
+  }
 });
 
 export default BottomSheet;
